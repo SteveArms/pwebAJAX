beaf89f6839bb4183b9b9c1d93ac7fa74c4ce84d
Anthony <acahuib@unsa.edu.pe>
2024-05-18 13:31:57 -0500

    Se arega la funcion para cargar los datos en la funcion de mostrarGrafico

--- Ejercicio6/script.js
+++ Ejercicio6/script.js
@@ -17,4 +17,38 @@ function toggleChartVisibility() {
             myChart.destroy();
         }
     }
+}function cargarDatos() {
+    return fetch('data.json').then(response => response.json())
+                             .then(data => data.filter(region => region.region !== 'Lima' && region.region !== 'Callao'));
+}
+
+function mostrarGrafico(data) {
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    const labels = data[0].confirmed.map(entry => entry.date);
+    const datasets = data.map(regionData => ({
+        label: `Valores para ${regionData.region}`,
+        data: regionData.confirmed.map(entry => parseInt(entry.value)),
+        borderColor: getRandomColor(),
+        borderWidth: 1
+    }));
+
+    myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
