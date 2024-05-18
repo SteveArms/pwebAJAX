437ec8dea9fc7abab251bba0bdf63f01b958f65f
Anthony <acahuib@unsa.edu.pe>
2024-05-18 13:33:09 -0500

    Funcion para tener colores aleatorios para los graficos

--- Ejercicio6/script.js
+++ Ejercicio6/script.js
@@ -52,3 +52,10 @@ function mostrarGrafico(data) {
         }
     });
 }
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgba(${r}, ${g}, ${b}, 1)`;
+}
+
