0c4a371ac0410211af50ebeba945efd1c8fadf7a
Anthony <acahuib@unsa.edu.pe>
2024-05-18 11:57:43 -0500

    Se arreglan errores al aceptar el boton

--- Ejercicio4/script.js
+++ Ejercicio4/script.js
@@ -1,19 +1,24 @@
-document.getElementById('mostrarGraficoBtn').addEventListener('click', cargarDatos);
+document.getElementById('mostrarGraficoBtn').addEventListener('click', function() {
+    cargarDatos().then(data => {
+        const arequipaData = data.find(region => region.region === 'Arequipa');
+        if (!arequipaData) {
+            console.error('No se encontraron datos para la región de Arequipa.');
+            return;
+        }
+
+        const fechas = arequipaData.confirmed.map(entry => entry.date);
+        const valores = arequipaData.confirmed.map(entry => parseInt(entry.value));
+
+        mostrarGrafico(fechas, valores);
+    }).catch(error => console.error('Error al cargar los datos:', error));
+});
 
 function cargarDatos() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            const arequipaData = data.find(region => region.region === 'Arequipa');
-            if (!arequipaData) throw new Error('No se encontraron datos para la región de Arequipa.');
-
-            const fechas = arequipaData.confirmed.map(entry => entry.date);
-            const valores = arequipaData.confirmed.map(entry => parseInt(entry.value));
-
-            mostrarGrafico(fechas, valores);
-        })
-        .catch(error => console.error('Error al cargar los datos:', error));
+    return fetch('data.json')
+        .then(response => response.json());
+	console.log("SI funciona");
 }
+
 function mostrarGrafico(fechas, valores) {
     document.getElementById('chartContainer').style.display = 'block';
 
@@ -38,3 +43,4 @@ function mostrarGrafico(fechas, valores) {
         }
     });
 }
+
