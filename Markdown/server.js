bed78763c3edf0391f3f387ab855ca594874846a
sllachod <sllachod@unsa.edu.pe>
2024-05-18 23:22:01 -0500

    puncion adicional para convertir markdown a html

--- Markdown/server.js
+++ Markdown/server.js
@@ -111,4 +111,19 @@ const server = http.createServer((req, res) => {
         };
         const contentType = mimeTypes[ext] || 'text/plain'; // Establecer el tipo de contenido según la extensión
         serveFile(res, filePath, contentType); // Servir el archivo
+    }
+
+    function markdownToHTML(markdown) {
+        // Función simple para convertir Markdown a HTML
+        return markdown
+          .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+          .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+          .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+          .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+          .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+          .replace(/\*(.*)\*/gim, '<i>$1</i>')
+          .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+          .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+          .replace(/\n$/gim, '<br />');
       }
+      
