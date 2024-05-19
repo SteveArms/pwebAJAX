136a05e88a6af031c2c3e1b9bc8f5af2641c15ec
sllachod <sllachod@unsa.edu.pe>
2024-05-18 23:10:25 -0500

    creando archivo main.js para las funciones del html

---
+++ Markdown/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fileList = document.getElementById('fileList');
+    const fileContent = document.getElementById('fileContent');
+    const createFileForm = document.getElementById('createFileForm');
+    const filenameInput = document.getElementById('filename');
+    const fileContentInput = document.getElementById('fileContentInput');
