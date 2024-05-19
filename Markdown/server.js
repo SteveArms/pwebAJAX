96639747ab5a1a2c6bf846f01a53f25b7fd8785f
sllachod <sllachod@unsa.edu.pe>
2024-05-18 22:14:37 -0500

    creando archivo server.js para crear servidor http, interaccion con los archivos, para rutas y manejar urls

---
+++ Markdown/server.js
@@ -0,0 +1,4 @@
+const http = require('http'); // Módulo para crear el servidor HTTP
+const fs = require('fs'); // Módulo para interactuar con el sistema de archivos
+const path = require('path'); // Módulo para manejar rutas de archivos
+const url = require('url'); // Módulo para manejar y parsear URLs
